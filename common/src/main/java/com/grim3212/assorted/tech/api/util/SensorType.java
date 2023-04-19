ce25f85b640b0f8664db6c084f0ce25541785ca3
Dakota Pierone <dakotapierone@yahoo.com>
2023-04-19 07:11:07 -0700

    Attempt to fix enum strings being inaccurate on some computers

--- common/src/main/java/com/grim3212/assorted/tech/api/util/SensorType.java
+++ common/src/main/java/com/grim3212/assorted/tech/api/util/SensorType.java
@@ -3,6 +3,7 @@ package com.grim3212.assorted.tech.api.util;
 import com.grim3212.assorted.lib.util.LibCommonTags;
 import com.grim3212.assorted.tech.api.TechTags;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.WaterAnimal;
@@ -18,35 +19,34 @@ import net.minecraft.world.level.material.Material;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-public enum SensorType {
-    WOOD(Material.WOOD, SoundType.WOOD, Entity.class, () -> Ingredient.of(ItemTags.PLANKS)),
-    STONE(Material.STONE, SoundType.STONE, LivingEntity.class, () -> Ingredient.of(LibCommonTags.Items.STONE)),
-    IRON(Material.METAL, SoundType.METAL, Player.class, () -> Ingredient.of(LibCommonTags.Items.STORAGE_BLOCKS_IRON)),
-    MOSSY_COBBLESTONE(Material.STONE, SoundType.STONE, Monster.class, () -> Ingredient.of(Blocks.MOSSY_COBBLESTONE)),
-    PRISMARINE(Material.STONE, SoundType.STONE, (ent) -> ent instanceof WaterAnimal || ent.getType().is(TechTags.Entities.SENSORS_WATER), () -> Ingredient.of(Blocks.PRISMARINE)),
-    GOLD(Material.METAL, SoundType.METAL, ItemEntity.class, () -> Ingredient.of(LibCommonTags.Items.STORAGE_BLOCKS_GOLD)),
-    EMERALD(Material.METAL, SoundType.METAL, AbstractVillager.class, () -> Ingredient.of(LibCommonTags.Items.STORAGE_BLOCKS_EMERALD)),
-    NETHERRACK(Material.STONE, SoundType.STONE, (ent) -> ent.getType().is(TechTags.Entities.SENSORS_NETHER), () -> Ingredient.of(LibCommonTags.Items.NETHERRACK)),
-    COBWEB(Material.STONE, SoundType.STONE, (ent) -> ent.getType().is(TechTags.Entities.SENSORS_ARTHROPODS), () -> Ingredient.of(LibCommonTags.Items.STRING)),
-    END_STONE(Material.STONE, SoundType.STONE, (ent) -> ent.getType().is(TechTags.Entities.SENSORS_END), () -> Ingredient.of(LibCommonTags.Items.END_STONES)),
-    HAY_BALE(Material.GRASS, SoundType.GRASS, (ent) -> ent.getType().is(TechTags.Entities.SENSORS_PETS), () -> Ingredient.of(Blocks.HAY_BLOCK)),
-    FEATHER(Material.WOOL, SoundType.WOOL, (ent) -> ent.getType().is(TechTags.Entities.SENSORS_FLYING), () -> Ingredient.of(LibCommonTags.Items.FEATHERS));
+public enum SensorType implements StringRepresentable {
+    WOOD("wood", Material.WOOD, SoundType.WOOD, Entity.class, () -> Ingredient.of(ItemTags.PLANKS)),
+    STONE("stone", Material.STONE, SoundType.STONE, LivingEntity.class, () -> Ingredient.of(LibCommonTags.Items.STONE)),
+    IRON("iron", Material.METAL, SoundType.METAL, Player.class, () -> Ingredient.of(LibCommonTags.Items.STORAGE_BLOCKS_IRON)),
+    MOSSY_COBBLESTONE("mossy_cobblestone", Material.STONE, SoundType.STONE, Monster.class, () -> Ingredient.of(Blocks.MOSSY_COBBLESTONE)),
+    PRISMARINE("prismarine", Material.STONE, SoundType.STONE, (ent) -> ent instanceof WaterAnimal || ent.getType().is(TechTags.Entities.SENSORS_WATER), () -> Ingredient.of(Blocks.PRISMARINE)),
+    GOLD("gold", Material.METAL, SoundType.METAL, ItemEntity.class, () -> Ingredient.of(LibCommonTags.Items.STORAGE_BLOCKS_GOLD)),
+    EMERALD("emerald", Material.METAL, SoundType.METAL, AbstractVillager.class, () -> Ingredient.of(LibCommonTags.Items.STORAGE_BLOCKS_EMERALD)),
+    NETHERRACK("netherrack", Material.STONE, SoundType.STONE, (ent) -> ent.getType().is(TechTags.Entities.SENSORS_NETHER), () -> Ingredient.of(LibCommonTags.Items.NETHERRACK)),
+    COBWEB("cobweb", Material.STONE, SoundType.STONE, (ent) -> ent.getType().is(TechTags.Entities.SENSORS_ARTHROPODS), () -> Ingredient.of(LibCommonTags.Items.STRING)),
+    END_STONE("end_stone", Material.STONE, SoundType.STONE, (ent) -> ent.getType().is(TechTags.Entities.SENSORS_END), () -> Ingredient.of(LibCommonTags.Items.END_STONES)),
+    HAY_BALE("hay_bale", Material.GRASS, SoundType.GRASS, (ent) -> ent.getType().is(TechTags.Entities.SENSORS_PETS), () -> Ingredient.of(Blocks.HAY_BLOCK)),
+    FEATHER("feather", Material.WOOL, SoundType.WOOL, (ent) -> ent.getType().is(TechTags.Entities.SENSORS_FLYING), () -> Ingredient.of(LibCommonTags.Items.FEATHERS));
 
+    private final String name;
     private final SoundType soundType;
     private final Predicate<? super Entity> trigger;
     private final Material material;
     private final Supplier<Ingredient> craftingMaterial;
 
-    private SensorType(Material material, SoundType soundType, Class<? extends Entity> trigger, Supplier<Ingredient> craftingMaterial) {
-        this.soundType = soundType;
-        this.trigger = (ent) -> {
+    private SensorType(String name, Material material, SoundType soundType, Class<? extends Entity> trigger, Supplier<Ingredient> craftingMaterial) {
+        this(name, material, soundType, (ent) -> {
             return trigger.isInstance(ent);
-        };
-        this.material = material;
-        this.craftingMaterial = craftingMaterial;
+        }, craftingMaterial);
     }
 
-    private SensorType(Material material, SoundType soundType, Predicate<? super Entity> trigger, Supplier<Ingredient> craftingMaterial) {
+    private SensorType(String name, Material material, SoundType soundType, Predicate<? super Entity> trigger, Supplier<Ingredient> craftingMaterial) {
+        this.name = name;
         this.soundType = soundType;
         this.trigger = trigger;
         this.material = material;
@@ -71,6 +71,11 @@ public enum SensorType {
 
     @Override
     public String toString() {
-        return this.name().toLowerCase();
+        return this.name;
+    }
+
+    @Override
+    public String getSerializedName() {
+        return this.name;
     }
 }
