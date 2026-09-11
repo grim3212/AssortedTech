2a93ecffb26fbf915c84b00c2608baa4929a9ac7
grim3212 <dakotapierone@yahoo.com>
2026-09-11 12:20:28 -0700

    Generate en_us.json with TechLanguageProvider
    Each block, item and entity is named from its id in title case unless the generator names it otherwise, so a new one needs no lang line; families that read differently are one rule each. The generated file matches the old hand-written one key for key.

--- neoforge/src/main/java/com/grim3212/assorted/tech/AssortedTechNeoForge.java
+++ neoforge/src/main/java/com/grim3212/assorted/tech/AssortedTechNeoForge.java
@@ -1,5 +1,6 @@
 package com.grim3212.assorted.tech;
 
+import com.grim3212.assorted.tech.client.data.TechLanguageProvider;
 import com.grim3212.assorted.lib.data.ForgeBlockTagProvider;
 import com.grim3212.assorted.lib.data.ForgeEntityTagProvider;
 import com.grim3212.assorted.lib.data.ForgeItemTagProvider;
@@ -66,5 +67,6 @@ public class AssortedTechNeoForge {
         event.addProvider(new TechBlockstateProvider(packOutput));
         event.addProvider(new TechItemModelProvider(packOutput));
         event.addProvider(new TechEquipmentAssetProvider(packOutput));
+        event.addProvider(new TechLanguageProvider(packOutput));
     }
 }
