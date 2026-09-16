8434c6724fc46691926e889c0b53bbe516557d14
grim3212 <dakotapierone@yahoo.com>
2026-09-16 06:42:02 -0700

    Support new instruction manual configuration

--- neoforge/src/main/java/com/grim3212/assorted/tech/client/data/TechManualProvider.java
+++ neoforge/src/main/java/com/grim3212/assorted/tech/client/data/TechManualProvider.java
@@ -3,15 +3,13 @@ package com.grim3212.assorted.tech.client.data;
 import com.grim3212.assorted.lib.data.LibManualProvider;
 import com.grim3212.assorted.lib.registry.IRegistryObject;
 import com.grim3212.assorted.tech.Constants;
-import com.grim3212.assorted.tech.api.util.SensorType;
-import com.grim3212.assorted.tech.api.util.SpikeType;
 import com.grim3212.assorted.tech.common.block.TechBlocks;
 import com.grim3212.assorted.tech.common.item.TechItems;
+import com.grim3212.assorted.tech.common.crafting.TechConditions;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.level.block.Block;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * This mod's section of the instruction manual. The spike and sensor families are read from the
@@ -35,53 +33,53 @@ public class TechManualProvider extends LibManualProvider {
     }
 
     private void addBridges() {
-        ChapterBuilder bridges = this.chapter("bridges");
+        ChapterBuilder bridges = this.chapter("bridges").whenPartEnabled(TechConditions.Parts.BRIDGES);
 
         // The beam itself is placed by the control, so it reads as the plain laser bridge.
-        bridges.recipes("laser", "bridge_control_laser")
+        bridges.recipes("laser", TechBlocks.BRIDGE_CONTROL_LASER.get())
                 .opens(TechBlocks.BRIDGE_CONTROL_LASER.get(), TechBlocks.BRIDGE.get());
-        bridges.recipes("trick", "bridge_control_trick").opens(TechBlocks.BRIDGE_CONTROL_TRICK.get());
-        bridges.recipes("accel", "bridge_control_accel").opens(TechBlocks.BRIDGE_CONTROL_ACCEL.get());
-        bridges.recipes("death", "bridge_control_death").opens(TechBlocks.BRIDGE_CONTROL_DEATH.get());
-        bridges.recipes("gravity", "bridge_control_gravity").opens(TechBlocks.BRIDGE_CONTROL_GRAVITY.get());
+        bridges.recipes("trick", TechBlocks.BRIDGE_CONTROL_TRICK.get()).opens(TechBlocks.BRIDGE_CONTROL_TRICK.get());
+        bridges.recipes("accel", TechBlocks.BRIDGE_CONTROL_ACCEL.get()).opens(TechBlocks.BRIDGE_CONTROL_ACCEL.get());
+        bridges.recipes("death", TechBlocks.BRIDGE_CONTROL_DEATH.get()).opens(TechBlocks.BRIDGE_CONTROL_DEATH.get());
+        bridges.recipes("gravity", TechBlocks.BRIDGE_CONTROL_GRAVITY.get()).opens(TechBlocks.BRIDGE_CONTROL_GRAVITY.get());
     }
 
     private void addGravity() {
         ChapterBuilder gravity = this.chapter("gravity");
 
-        gravity.recipes("attractors", "attractor", "attractor_directional").every(60)
+        gravity.recipes("attractors", TechBlocks.ATTRACTOR.get(), TechBlocks.ATTRACTOR_DIRECTIONAL.get()).whenPartEnabled(TechConditions.Parts.GRAVITY).every(60)
                 .opens(TechBlocks.ATTRACTOR.get(), TechBlocks.ATTRACTOR_DIRECTIONAL.get());
-        gravity.recipes("repulsors", "repulsor", "repulsor_directional").every(60)
+        gravity.recipes("repulsors", TechBlocks.REPULSOR.get(), TechBlocks.REPULSOR_DIRECTIONAL.get()).whenPartEnabled(TechConditions.Parts.GRAVITY).every(60)
                 .opens(TechBlocks.REPULSOR.get(), TechBlocks.REPULSOR_DIRECTIONAL.get());
-        gravity.recipes("gravitors", "gravitor", "gravitor_directional").every(60)
+        gravity.recipes("gravitors", TechBlocks.GRAVITOR.get(), TechBlocks.GRAVITOR_DIRECTIONAL.get()).whenPartEnabled(TechConditions.Parts.GRAVITY).every(60)
                 .opens(TechBlocks.GRAVITOR.get(), TechBlocks.GRAVITOR_DIRECTIONAL.get());
-        gravity.recipes("boots", "gravity_boots").opens(TechItems.GRAVITY_BOOTS.get());
+        gravity.recipes("boots", TechItems.GRAVITY_BOOTS.get()).opens(TechItems.GRAVITY_BOOTS.get());
     }
 
     private void addSensors() {
-        String[] recipes = Stream.of(SensorType.values()).map(type -> type + "_sensor").toArray(String[]::new);
+        Block[] all = blocks(TechBlocks.SENSORS);
 
-        ChapterBuilder sensors = this.chapter("sensors");
-        sensors.recipes("sensors", recipes).every(60).opens(blocks(TechBlocks.SENSORS));
+        ChapterBuilder sensors = this.chapter("sensors").whenPartEnabled(TechConditions.Parts.SENSORS);
+        sensors.recipes("sensors", all).every(60).opens(all);
         sensors.text("triggers");
     }
 
     private void addSpikes() {
-        String[] recipes = Stream.of(SpikeType.values()).map(type -> type + "_spike").toArray(String[]::new);
+        Block[] all = blocks(TechBlocks.SPIKES);
 
-        ChapterBuilder spikes = this.chapter("spikes");
-        spikes.recipes("spikes", recipes).every(50).opens(blocks(TechBlocks.SPIKES));
+        ChapterBuilder spikes = this.chapter("spikes").whenPartEnabled(TechConditions.Parts.SPIKES);
+        spikes.recipes("spikes", all).every(50).opens(all);
         spikes.text("materials");
     }
 
     private void addRedstone() {
         ChapterBuilder redstone = this.chapter("redstone");
 
-        redstone.recipes("fan", "fan").opens(TechBlocks.FAN.get());
-        redstone.recipes("alarm", "alarm").opens(TechBlocks.ALARM.get());
-        redstone.recipes("glowstone_torch", "glowstone_torch")
+        redstone.recipes("fan", TechBlocks.FAN.get()).whenPartEnabled(TechConditions.Parts.FAN).opens(TechBlocks.FAN.get());
+        redstone.recipes("alarm", TechBlocks.ALARM.get()).whenPartEnabled(TechConditions.Parts.ALARM).opens(TechBlocks.ALARM.get());
+        redstone.recipes("glowstone_torch", TechBlocks.GLOWSTONE_TORCH.get()).whenPartEnabled(TechConditions.Parts.TORCHES)
                 .opens(TechBlocks.GLOWSTONE_TORCH.get(), TechBlocks.GLOWSTONE_WALL_TORCH.get());
-        redstone.recipes("flip_flop_torch", "flip_flop_torch")
+        redstone.recipes("flip_flop_torch", TechBlocks.FLIP_FLOP_TORCH.get()).whenPartEnabled(TechConditions.Parts.TORCHES)
                 .opens(TechBlocks.FLIP_FLOP_TORCH.get(), TechBlocks.FLIP_FLOP_WALL_TORCH.get());
     }
 
